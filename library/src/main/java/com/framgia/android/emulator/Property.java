7cda748ab991c939b00c7df2a565720aabe0f7be
KaKaVip <haipq@ymeo.net>
2016-05-17 09:29:43 +0700

    Add builder param and new function checker

---
+++ library/src/main/java/com/framgia/android/emulator/Property.java
@@ -0,0 +1,14 @@
+package com.framgia.android.emulator;
+
+/**
+ * Created by pham.quy.hai on 5/17/16.
+ */
+class Property {
+    public String name;
+    public String seek_value;
+
+    public Property(String name, String seek_value) {
+        this.name = name;
+        this.seek_value = seek_value;
+    }
+}
